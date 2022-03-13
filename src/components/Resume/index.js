c6ad37ad1973ed79150761dbc196394df5b647f2
box-monkey <jr.gasparin15@gmail.com>
2022-03-13 04:25:35 -0400

    added project images, skills under resume tab

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
 
 function Resume() {
-return(
-    <p>I'm a resume</p>
-)
+  return (
+    <section className="resume-section">
+      <div className="resume-container">
+        <h1>Skills</h1>
+        {/* <br/> */}
+
+        <h2>Front End</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>jQuery</li>
+          <li>Bootstrap</li>
+          <li>Tailwind</li>
+          <li>React</li>
+        </ul>
+
+        <br />
+        <h2>Back End</h2>
+        <ul>
+          <li>APIs</li>
+          <li>Node.Js</li>
+          <li>Express</li>
+          <li>MySQL / Sequelize</li>
+          <li>MondoDB / Mongoose</li>
+          <li>REST</li>
+          <li>MERN</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default Resume;
