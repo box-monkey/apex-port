ff0c415f7c41155afdce0daf53c9c6798692371d
box-monkey <jr.gasparin15@gmail.com>
2022-03-13 03:36:35 -0400

    added footer styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+import "./Footer.css";
 
-function Footer(){
-return(
-    <p>hey im a footer</p>
-)
-};
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li className="footer-li">
+          <a
+            className="footer-link"
+            href="https://github.com/box-monkey"
+            target="_blank"
+          >
+            Github
+          </a>
+        </li>
+        <li className="footer-li">
+          <a
+            className="footer-link"
+            // create linked in profile and add
+            href="https://www.linkedin.com"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
 
 export default Footer;
