d7bb21510bd9e7d44c0f06f24766d5e67ae3ecfe
box-monkey <jr.gasparin15@gmail.com>
2022-03-14 01:07:12 -0400

    just need to style

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,26 +13,51 @@ function Portfolio() {
     <section>
       <Projects
         name="Techblog"
-        repo="ghlogo"
+        repo={ghlogo}
         githubLink="https://github.com/box-monkey/apex-blogtech"
         githubDeploy="https://apex-blogtech.herokuapp.com/"
         projImg={apex1}
       ></Projects>
 
-      
-      {/* <Projects
+      <Projects
         name="getfit"
+        repo={ghlogo}
         githubLink="https://github.com/box-monkey/git-fit"
         githubDeploy="https://kkkaran.github.io/Get-Fit-App/"
         projImg={getfit}
       ></Projects>
-        <Projects
+
+      <Projects
         name="Weather Forecast"
+        repo={ghlogo}
         githubLink="https://box-monkey.github.io/digital-samurai/"
         githubDeploy="https://github.com/box-monkey/digital-samurai"
         projImg={weatherapp}
-      ></Projects> */}
+      ></Projects>
 
+      <Projects
+        name="Notetaker"
+        repo={ghlogo}
+        githubLink="https://github.com/box-monkey/apex-note-taker"
+        githubDeploy="https://apex-note-taker.herokuapp.com/"
+        projImg={notetaker}
+      ></Projects>
+
+      <Projects
+        name="Sellquicker"
+        repo={ghlogo}
+        githubLink="https://github.com/KKkaran/sell-quicker"
+        githubDeploy="https://sellquicker.herokuapp.com/"
+        projImg={sellquicker}
+      ></Projects>
+
+      <Projects
+        name="e-commerce back end"
+        repo={ghlogo}
+        githubLink="https://github.com/box-monkey/apex-commerce-rear-end"
+        githubDeploy="https://watch.screencastify.com/v/CA2NbsS2Ay4jdTykwYId"
+        projImg={ecom}
+      ></Projects>
     </section>
   );
 }
