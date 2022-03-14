507a1fe344eeea8ec6397db71d8618b0cf1b5cc4
box-monkey <jr.gasparin15@gmail.com>
2022-03-14 04:13:59 -0400

    about appears better on mobile

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,13 +8,14 @@ function About() {
       <h2 className="section-title">About Me</h2>
       <img src={me} alt="me" className="me" />
 
-      <h4>
+      <h4 className="section-text">
         Hi, I'm Roberto Gasparin, I'm a full stack developer.
         <br />
         My passion is building and creating art. I am eager to accumulate, share
-        and implement knowledge. Attended a rigorous bootcamp from the
-        university of Toronto in collaberation with triOS college. In which I
-        was able to gather full stack application capabilites.
+        and
+        <br /> implement knowledge. Attended a rigorous bootcamp from the
+        university of Toronto <br /> in collaberation with triOS college. In
+        which I was able to gather full stack application capabilites.
         <br />
         Let's build things on the internet.
       </h4>
